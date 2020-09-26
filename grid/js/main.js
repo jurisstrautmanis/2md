12f3274abe9fbb05a1bf0a6050657bdc6e31b30b
Juris Strautmanis <jurisstrautmanis@Juriss-MacBook-Pro.local>
2020-09-26 22:02:25 +0300

    grids plus time

---
+++ grid/js/main.js
@@ -0,0 +1,14 @@
+//Pulkstenis lapā šis ir
+setInterval(() => {
+  let d = new Date()
+  let hours = d.getHours()
+  let minutes = d.getMinutes()
+  let seconds = d.getSeconds()
+  let out = `${a(hours)}:${a(minutes)}:${a(seconds)}`
+  const el = document.getElementById('user')
+  el.innerHTML = out
+}, 1000);
+
+function a(b){
+  return b < 10 ? `0${b}` : b
+}
